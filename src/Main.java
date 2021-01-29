ae940ea0536ecaa7382fbcd9644a51d860268dc2
AbdusadikSattar <71940275+AbdusadikSattar@users.noreply.github.com>
2021-01-29 05:43:30 +0100

    Added some code to the file

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,10 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello");
-        
+
+        int i;
+        for(i=0;i<10;i++){
+            System.out.println(i);
+        }
     }
 }
